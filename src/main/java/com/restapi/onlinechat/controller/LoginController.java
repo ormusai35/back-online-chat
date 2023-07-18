1469f20ecf52ee06bbe14bfd1ade16451c1ea63c
Ormusai35 <ormusai35@gmail.com>
2023-07-18 14:04:35 +0300

    add signUp

--- src/main/java/com/restapi/onlinechat/controller/LoginController.java
+++ src/main/java/com/restapi/onlinechat/controller/LoginController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +20,6 @@ public class LoginController {
 	
 	@PostMapping(path="create-user")
 	public ResponseEntity<User> createUser(@RequestBody User user) {
-		return new ResponseEntity<>(userService.saveUser(user), HttpStatus.OK);
+		return new ResponseEntity<User>(userService.saveUser(user), HttpStatus.OK);
 	}
 }
--- src/main/java/com/restapi/onlinechat/service/UserService.java
+++ src/main/java/com/restapi/onlinechat/service/UserService.java
@@ -2,7 +2,6 @@ package com.restapi.onlinechat.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.restapi.onlinechat.model.User;
 import com.restapi.onlinechat.repository.UserRepository;
 
@@ -15,5 +14,7 @@ public class UserService {
 	public User saveUser(User user) {
 		return userRepository.save(user);
 	}
+	
+	
 
 }
